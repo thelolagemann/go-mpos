31ec91ec618b818cd74b9d6c649bb201d32e5029
Harry Bond <15960568+thelolagemann@users.noreply.github.com>
2021-03-04 20:36:11 +0000

    less verbose user funcs

--- mpos_user.go
+++ mpos_user.go
@@ -1,7 +1,6 @@
 package mpos
 
 import (
-	"net/http"
 	"net/url"
 )
 
@@ -14,44 +13,39 @@ type MiningPoolHubUser struct {
 	id int32
 }
 
-func (m *MiningPoolHubUser) getUser(action string, bind interface{}) (*http.Response, error) {
+func (m *MiningPoolHubUser) getUser(action string, bind interface{}) error {
 	params := &url.Values{}
 	params.Add("id", string(m.id))
-	return m.get(action, params, bind)
+	_, err := m.get(action, params, bind)
+	return err
 }
 
 // Balance fetch the users balance.
 func (m *MiningPoolHubUser) Balance() (uRes *UserBalanceResponse, err error) {
-	_, err = m.getUser("getuserbalance", &uRes)
-	return uRes, err
+	return uRes, m.getUser("getuserbalance", &uRes)
 }
 
 // Hashrate fetch the users hash rate.
 func (m *MiningPoolHubUser) Hashrate() (uRes float64, err error) {
-	_, err = m.getUser("getuserhashrate", &uRes)
-	return uRes, err
+	return uRes, m.getUser("getuserhashrate", &uRes)
 }
 
 // Sharerate fetch the users share rate.
 func (m *MiningPoolHubUser) Sharerate() (uRes float64, err error) {
-	_, err = m.getUser("getusersharerate", &uRes)
-	return uRes, err
+	return uRes, m.getUser("getusersharerate", &uRes)
 }
 
 // Status fetch the users overall status.
 func (m *MiningPoolHubUser) Status() (uRes UserStatusResponse, err error) {
-	_, err = m.getUser("getuserstatus", &uRes)
-	return uRes, err
+	return uRes, m.getUser("getuserstatus", &uRes)
 }
 
 // Transactions get the users transactions.
 func (m *MiningPoolHubUser) Transactions() (uRes UserTransactionsResponse, err error) {
-	_, err = m.getUser("getusertransactions", &uRes)
-	return uRes, err
+	return uRes, m.getUser("getusertransactions", &uRes)
 }
 
 // Workers fetch the users worker status.
 func (m *MiningPoolHubUser) Workers() (uRes UserWorkersResponse, err error) {
-	_, err = m.getUser("getuserworkers", &uRes)
-	return uRes, err
+	return uRes, m.getUser("getuserworkers", &uRes)
 }
